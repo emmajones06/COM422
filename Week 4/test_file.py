8013c8bd22ec27bc22aa1bda770edf469802306b
0jonee37 <0jonee37@solent.ac.uk>
2025-02-11 10:54:37 +0000

    kennels

---
+++ Week 4/test_file.py
@@ -0,0 +1,26 @@
+from dog import Dog
+from enclosure import Enclosure
+from kennel_company import Kennel_Company
+import pytest
+
+@pytest.fixture
+def kennel_with_dogs(monkeypatch):
+
+
+def test_book_dog():
+    # Create a kennel company
+
+    # Book 2 dogs in to the kennels
+
+    # Check that the overall number of spaces left in the kennels is correct after the 2 bookings
+
+
+def test_remove_dog():
+    # Create a kennel company
+
+    # The kennel company will need to have at least 1 dog staying with it - facilitate this
+
+    # Remove the dog
+
+    # Check that the overall number of spaces left is correct after the removal
+
